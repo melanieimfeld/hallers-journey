285be60349bd7005c8e49e2f2f4c77b61eabc219
sand <alexander.belokon@mapbox.com>
2022-07-10 04:10:01 +0300

    feat: streamline and debounce scroll.progress

--- src/util/scroll.js
+++ src/util/scroll.js
@@ -8,8 +8,8 @@ export const actions = {
     },
     enter: (response, config, map) => {
         const { index, element } = response;
-        element.classList.add('active');    
-        
+        element.classList.add('active');
+
         [...element.querySelectorAll('.icon')].forEach((item, idx) => {
             item.style.animationTimingFunction = "ease-in-out";
             item.style.animationDelay = `${idx * 0.1}s`;
@@ -23,47 +23,41 @@ export const actions = {
         }
     },
     progress: (response, config, map, data) => {
-
         const { index, progress, element } = response;
-        let stepProgress;
-        let movingLine;
-    
-        const chapter = config.chapters.find(
-            (chap) => chap.id === response.index
-        );
-    
-    
-        if (element.className.includes('header')) {
-            response.element.style.opacity = Math.round(1 - response.progress);
-        }
-    
-        if (element.className.includes('left') && chapter.hasPath) {
-            //stepProgress = Math.round(response.progress * config.driveSmoothness + config.driveSmoothness*driveSlideNum);
-            stepProgress = Math.round(progress * config.driveSmoothness);
-    
-            movingLine = changeCenter(
-                stepProgress,
-                index - 1,
-                data
-            );
+        const chapter = config.chapters.find(chapter => chapter.id === index);
 
-            map.getSource('lineSource').setData(movingLine);
-            if (config.followPoint && Math.floor(progress) % 0.6 === 0 ) {
-                map.easeTo({
-                    center: movingLine.features[0].geometry.coordinates[movingLine.features[0].geometry.coordinates.length - 1],
-                    zoom: 13,
-                    bearing: chapter.location.bearing,
-                    pitch: 65
-                });
-            }
+        if (element.className.includes('header')) {
+            element.style.opacity = Math.round(1 - progress);
         }
-            
+
+        const setCenter = center => {
+            if (!config.followPoint || !center) return;
+
+            const { lng, lat } = map.getCenter()
+            const previous = [lng, lat]
+            const round = x => Math.round(x * 1e3)
+            const isSame = String(previous.map(round)) === String(center.map(round))
+            if (isSame) return;
+
+            map.easeTo({ center, zoom: 13, bearing: chapter.location.bearing, pitch: 65 });
+        }
+
+        const finishedSteps = Math.max(0, index - 1)
+        const stepProgress = !index ? 0 : Math.round(progress * config.driveSmoothness);
+        const movingLine = changeCenter(stepProgress, finishedSteps, data);
+        map.getSource('lineSource').setData(movingLine);
+
+        if (element.className.includes('chapter')) {
+            const { features: [{ geometry }] } = movingLine
+            setCenter(geometry.coordinates.slice(-1)[0])
+        }
+
         if (element.className.includes('footer') && map.getPitch() !== 0) {
             map.setPitch(0);
             map.fitBounds([
                 [7.411651611328124, 46.93291653811045],
                 [7.922859191894531, 47.0284823920254]
             ], { bearing: 76, padding: map.getPadding() });
+        }
     }
-}
 }
